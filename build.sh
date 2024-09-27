49ba8630f4ede0ccb8e017adbe50409881d5d795
esteban <estebanquinae@gmail.com>
2024-09-27 18:08:14 -0500

    upgredes de build con bachend

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-source venv/bin/activate
-pip install --upgrate pip
+
+source .venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-#API_URL=https://dockercrey.up.railway.app/ 
+API_URL=https://program-web.up.railway.app/ 
 reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
-deactivated
+deactivate
--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="complete",
+    
+    cors_allowed_origins=[
+        "http://localhost:3000",
+       "https://programcreyente.vercel.app/"
+   ]
 )
